6fdbff915ace5665928dcdbe4928ee5121c6deec
mvasquezes <matiasvasquezescobar@gmail.com>
2026-09-02 15:10:04 -0400

    PersistenciaDeDatos

---
+++ app/(stack)/home/index.tsx
@@ -0,0 +1,53 @@
+import CustomButton from '@/components/shared/CustomButton';
+import { router } from 'expo-router';
+import { View } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View className="px-10 mt-5">
+        <CustomButton
+          className="mb-2"
+          color="primary"
+          onPress={() => router.push('./products')}
+        >
+          Productos
+        </CustomButton>
+{/*
+        <CustomButton
+          onPress={() => router.push('/profile')}
+          className="mb-2"
+          color="secondary"
+        >
+          Profile
+        </CustomButton>
+
+        <CustomButton
+          onPress={() => router.push('/settings')}
+          className="mb-2"
+          color="tertiary"
+        >
+          Ajustes
+        </CustomButton>*/}
+
+       {/* {<Link href="/products" asChild>
+          <CustomButton variant="text-only" className="mb-10" color="primary">
+            Productos
+          </CustomButton>
+        </Link>}*/}
+
+        {/* <Link className="mb-5" href="/products">
+          Productos{' '}
+        </Link>
+        <Link className="mb-5" href="/profile">
+          Perfil{' '}
+        </Link>
+        <Link className="mb-5" href="/settings">
+          Ajustes{' '}
+        </Link> */}
+      </View>
+    </SafeAreaView>
+  );
+};
+export default HomeScreen;
---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import { Redirect } from 'expo-router';
+
+const App=()=>{
+return <Redirect href="/(stack)/home" />;
+  /*
+    return(
+      <SafeAreaView>
+
+  <View className="mt-6 mx-2.5">
+        <Text className="text-xl font-bold text-green-500 ">
+        Welcom tsdsdoivewi!
+        </Text>
+       
+        <Text className="text-2xl font-work-black">
+          otro texto
+        </Text>
+        <Text className="text-2xl font-work-medium">
+          otro texto
+        </Text>
+        <Text className="text-2xl font-work-light">
+          otro texto
+        </Text>
+     
+     
+      
+        </View>
+      </SafeAreaView>
+       
+     
+     
+    ) */
+}
+export default App
